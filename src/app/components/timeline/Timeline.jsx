68c402cc2919a7cfba0f0424fca2b45167ae10d6
rpmonteiro <ricpmonteiro@gmail.com>
2017-04-10 20:27:39 +0100

    Add timeline skeleton component and basic ruler grid

---
+++ src/app/components/timeline/Timeline.jsx
@@ -0,0 +1,28 @@
+import React, { PropTypes, PureComponent } from 'react';
+import TimelineRuler from './components/TimelineRuler';
+
+export default class Timeline extends PureComponent {
+
+  static propTypes = {
+    
+  }
+  
+  state = {
+    
+  }
+  
+  componentWillMount() {
+    
+  }
+  
+
+  render() {
+    
+    
+    return (
+      <div className="timeline">
+        <TimelineRuler />
+      </div>
+    );
+  }
+}
